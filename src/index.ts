496251fae2270ee4e127e87e093d4581c32a2ac1
nth-alex <nth.alexx@gmail.com>
2026-05-03 13:47:17 +0300

    chore: export layout and navigation components from package index

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,14 @@ export { Button } from './components/Button/Button'
 export { Input } from './components/Input/Input'
 export { Card } from './components/Card/Card'
 
+// Layout & Navigation (phase 4)
+export { DashboardLayout } from './components/DashboardLayout/DashboardLayout'
+export { Sidebar } from './components/Sidebar/Sidebar'
+export { Header } from './components/Header/Header'
+export { NavItem } from './components/NavItem/NavItem'
+export { NavGroup } from './components/NavGroup/NavGroup'
+export { PageContent } from './components/PageContent/PageContent'
+
 // Theme system
 export {
   ThemeProvider,
